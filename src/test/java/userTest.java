1edab121d15337465e5fa1c6f606901f1da40cf7
DONALOMAOILCHIARAIN- <g00365724@atu.ie>
2025-04-24 14:43:30 +0100

    added connection driver in main

--- src/test/java/userTest.java
+++ src/test/java/userTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class userTest {
+
     @Test
     void testUserConstructorAndGetters() {
         User user = new User(1, "Anna_Pierce", "password123", "Doctor");
