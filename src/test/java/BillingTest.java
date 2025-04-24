4079889eecf029647f17486c5d5b28737f5630b5
G00395858@atu.ie <G00395858@atu.ie>
2025-04-24 14:11:18 +0100

    Added Junit Testing for Appointment, patient and billing

---
+++ src/test/java/BillingTest.java
@@ -0,0 +1,27 @@
+package ie.atu.week10;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class BillingTest {
+
+    @Test
+    public void testBillingCreation() {
+        Billing bill = new Billing(1001, 1, 250.00, "Unpaid");
+        assertEquals(1001, bill.getBillID());
+        assertEquals(1, bill.getPatientID());
+        assertEquals(250.00, bill.getAmount());
+        assertEquals("Unpaid", bill.getPaymentStatus());
+    }
+
+    @Test
+    public void testBillingSetters() {
+        Billing bill = new Billing(1001, 1, 250.00, "Unpaid");
+        bill.setAmount(300.00);
+        bill.setPaymentStatus("Paid");
+
+        assertEquals(300.00, bill.getAmount());
+        assertEquals("Paid", bill.getPaymentStatus());
+    }
+}
+
