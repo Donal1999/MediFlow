6daa19c8c08d98a3e83a903534394217d369bf33
Mia Acton <G00428601@atu.ie>
2025-04-27 23:27:20 +0100

    Adjusted accepted parameters for patient class and included minor updates to overall code to rectify compiliation errors

--- src/main/java/ie/atu/week10/Appointment.java
+++ src/main/java/ie/atu/week10/Appointment.java
@@ -4,26 +4,26 @@ import java.util.Date;
 
 public class Appointment {
     private int patientID;
-    private int doctorDr;
+    private int doctorID;
     private Date appointmentDate;
 
     //Constructor
-    public Appointment(int patientID, int doctorDr, Date appointmentDate) {
+    public Appointment(int patientID, int doctorID, Date appointmentDate) {
         this.patientID = patientID;
-        this.doctorDr = doctorDr;
+        this.doctorID = doctorID;
         this.appointmentDate = appointmentDate;
     }
 
     // Getters+Setters
     public int getPatientID() { return patientID; }
-    public int getDoctorID() { return doctorDr; }
+    public int getDoctorID() { return doctorID; }
 
     public Date getAppointmentDate() { return appointmentDate; }
     public void setAppointmentDate(Date appointmentDate) { this.appointmentDate = appointmentDate; }
 
     //display appointment details
     public void displayAppointment() {
-        System.out.println("Date: " + appointmentDate + ", Patient ID: " + patientID + ", Doctor ID: " + doctorDr);
+        System.out.println("Date: " + appointmentDate + ", Patient ID: " + patientID + ", Doctor ID: " + doctorID);
     }
 }
 
